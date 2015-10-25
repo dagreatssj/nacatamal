a14d4a295320bee50ed09660209c19d5b48b9a1b
Darrell Calderon <dagreatssj@gmail.com>
2015-10-25 14:49:47 -0700

    I had the parameter on the wrong side

--- external/php_deps_install.sh
+++ external/php_deps_install.sh
@@ -2,8 +2,8 @@
 
 if [ -f /etc/redhat-release ]; then
 	sudo yum update
-	sudo yum install -y libxml2-devel pcre-devel curl-devel openssl-devel ncurses-devel
+	sudo yum -y install libxml2-devel pcre-devel curl-devel openssl-devel ncurses-devel
 else
 	sudo apt-get update
-	sudo apt-get install -y libxml2-devel pcre-devel curl-devel openssl-devel ncurses-devel
+	sudo apt-get -y install libxml2-devel pcre-devel curl-devel openssl-devel ncurses-devel
 fi
