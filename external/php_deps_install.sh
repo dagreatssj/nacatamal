7dba87ecb5bd22a5b364ce5977fb2dd2a0ba76d8
Darrell Calderon <dagreatssj@gmail.com>
2016-01-24 17:42:41 -0800

    correcting the openssl package for ubuntu

--- external/php_deps_install.sh
+++ external/php_deps_install.sh
@@ -5,5 +5,5 @@ if [ -f /etc/redhat-release ]; then
 	sudo yum install -y libxml2-devel pcre-devel curl-devel openssl-devel ncurses-devel
 else
 	sudo apt-get update -y
-	sudo apt-get install -y libxml2-dev libpcre3-dev libcurl-dev libssl-dev libncurses-dev
+	sudo apt-get install -y libxml2-dev libpcre3-dev libcurl4-openssl-dev libssl-dev libncurses-dev
 fi
